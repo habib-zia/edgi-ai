79e8076b99d93b65f68b3d6e8236a9efeb7420e9
Amir Ali <aali@techtiz.co>
2025-11-06 14:48:11 +0500

    build issue fixes

--- src/components/ui/auth-modal-wrapper.tsx
+++ src/components/ui/auth-modal-wrapper.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRef, useEffect, useCallback, ReactNode } from 'react'
+import { useRef, useEffect, ReactNode } from 'react'
 
 interface AuthModalWrapperProps {
   isOpen: boolean
